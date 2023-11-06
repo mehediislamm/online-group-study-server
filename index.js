456741d7fb891d9ae73511dacf5486d181b3b0d9
mehedi505991 <mdmehediislammoksedul@gmail.com>
2023-11-06 12:45:43 +0600

    created get and post method and hide user name and password

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
-
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -10,8 +10,8 @@ app.use(express.json());
 
 
 
-// const uri = "mongodb+srv://<username>:groupStudy@OF4ynwLVq7ModC0T.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb+srv://groupStudy:OF4ynwLVq7ModC0T@cluster0.zjauaf8.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://<username>:groupStudy@OF4ynwLVq7ModC0T.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjauaf8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +27,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const assignmentsCollection = client.db('group-studyDB').collection('assignments');
+    const featureCollection = client.db('group-studyDB').collection('features');
+    const submitedCollection = client.db('group-studyDB').collection('submitedassignment');
 
 
     app.post('/api/v1/create-assignments', async(req,res)=>{
@@ -35,7 +37,8 @@ async function run() {
         res.send(result)
     })
 
-  
+   
+
     app.get("/api/v1/all-assignment", async (req, res) => {
       const result = await assignmentsCollection.find().toArray();
       res.send(result);
@@ -56,7 +59,7 @@ async function run() {
             description: data.description,
             date: data.date,
             image:data.image,
-            level: data.data
+            level: data.level
           },
         };
 
@@ -79,6 +82,24 @@ async function run() {
         console.log(result);
         res.send(result);
       });
+
+      // update single user end
+
+
+        // features card section
+        app.get("/api/v1/features-cards", async (req, res) => {
+          const result = await featureCollection.find().toArray();
+          res.send(result);
+        });
+
+
+        // submited assignment
+        app.post('/api/v1/submited-assignments', async(req,res)=>{
+          const assignments = req.body;
+          const result = await submitedCollection.insertOne(assignments)
+          res.send(result)
+      })
+
   
 
 
